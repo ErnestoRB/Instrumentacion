61bba4a7753f57db0a143b92855dcab83be5a069
Ernesto Ramírez <ernestorb_@outlook.com>
2024-02-13 17:39:44 -0600

    Agregar cambios en codigo practica laboratorio 1

--- Laboratorio/Practica1/Practica1.ino
+++ Laboratorio/Practica1/Practica1.ino
@@ -3,17 +3,20 @@
 #include <DHT_U.h>
 #define SCROLL_SPEED 500
 #define DATA_SPEED 1000
-double tempC;     // Variable para almacenar temperatura
-double humidity;     // Variable para almacenar humedad
-int pinDHT11 = 8;  // Variable del pin de entrada del sensor DHT
+#define RELAY_PIN 9
+
+double tempC = 0;     // Variable para almacenar temperatura
+double humidity = 0;  // Variable para almacenar humedad
+int pinDHT11 = 8;     // Variable del pin de entrada del sensor DHT
 unsigned long currentMilis = 0;
 unsigned long previousUpdate = 0;
 
-DHT dht(pinDHT11,DHT11);
+DHT dht(pinDHT11, DHT11);
 
 LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
 
 void setup() {
+  pinMode(RELAY_PIN, OUTPUT);
   Serial.begin(9600);
   dht.begin();
   lcd.begin(16, 2);
@@ -27,6 +30,11 @@ void loop() {
   if (currentMilis - previousUpdate >= DATA_SPEED) {
     tempC = calcularTemperatura();
     humidity = calcularHumedad();
+    if (humidity > 50) {
+      digitalWrite(RELAY_PIN, LOW);
+    } else {
+      digitalWrite(RELAY_PIN, HIGH);
+    }
     imprimirSerial();
     printScreen();
     previousUpdate = currentMilis;
@@ -34,6 +42,7 @@ void loop() {
 }
 
 void printScreen() {
+  lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Temp.   ");
   lcd.print(tempC);
@@ -55,6 +64,6 @@ double calcularHumedad() {
 void imprimirSerial() {
   Serial.print("\nTemperatura:");
   Serial.print(tempC);
-  Serial.print("\nHumedad:");                       
+  Serial.print("\nHumedad:");
   Serial.print(humidity);
 }
